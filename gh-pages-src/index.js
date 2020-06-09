2c0ae6a10338d9554b1ec9400661d8ed2d6f04d3
Davit Yavryan <davit.yavryan@gmail.com>
2020-06-10 01:20:11 +0200

    Fix development build for devs who use case sensitive FS

--- gh-pages-src/index.js
+++ gh-pages-src/index.js
@@ -1,6 +1,6 @@
 // Use the window load event to keep the page load performance
 window.addEventListener('load', () => {
-    import(/* webpackChunkName: "pages/index" */'Pages/Index').then(({ default: loadIndex }) => {
+    import(/* webpackChunkName: "pages/index" */'Pages/Index/Index').then(({ default: loadIndex }) => {
         loadIndex();
     });
 });
