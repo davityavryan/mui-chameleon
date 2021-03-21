d7eb8b94632c6aae0c558f950e434bbbfa59e07d
Davit Yavryan <davit.yavryan@gmail.com>
2021-03-21 22:18:40 +0100

    test: improve BooleanEditor test

--- src/internal/BooleanEditor/BooleanEditor.unit.tsx
+++ src/internal/BooleanEditor/BooleanEditor.unit.tsx
@@ -6,51 +6,30 @@ import '../../../cypress'; // Add custom commands
 
 import BooleanEditor from './BooleanEditor';
 
-context('BooleanEditor', () => {
-    describe('should', () => {
-        it('have correct label', () => {
-            const themeKey = 'parents-parent.parent.child';
-            const onChange = cy.spy();
+describe('BooleanEditor should', () => {
+    const themeKey = 'parents-parent.parent.child';
 
-            const Icon0 = () => <span id="icon0">icon-left</span>;
-            const Icon1 = () => <span id="icon1">icon-right</span>;
+    const Icon0 = () => <span id="icon0">icon-left</span>;
+    const Icon1 = () => <span id="icon1">icon-right</span>;
 
-            mount(
-                <BooleanEditor
-                    options={['left', 'right']}
-                    icons={[Icon0, Icon1]}
-                    value={'left'}
-                    themeKey={themeKey}
-                    onChange={onChange}
-                />
-            );
+    const options = ['left', 'right'];
+    const icons = [Icon0, Icon1];
 
-            cy.getByTestId('boolean-editor-label').should('exist').should('have.text', 'Child');
-        });
+    beforeEach(() => {
+        const onChange = cy.spy().as('onChange');
 
-        it('change value on icon click', () => {
-            const themeKey = 'parents-parent.parent.child';
-            const onChange = cy.spy().as('onChange');
+        mount(
+            <BooleanEditor options={options} icons={icons} value={options[0]} themeKey={themeKey} onChange={onChange} />
+        );
+    });
 
-            const Icon0 = () => <span id="icon0">icon-left</span>;
-            const Icon1 = () => <span id="icon1">icon-right</span>;
+    it('have correct label', () => {
+        cy.getByTestId('boolean-editor-label').should('exist').should('have.text', 'Child');
+    });
 
-            const options = ['left', 'right'];
-            const icons = [Icon0, Icon1];
+    it('change value on icon click', () => {
+        cy.getByTestId('boolean-editor-toggle').should('exist').click();
 
-            mount(
-                <BooleanEditor
-                    options={options}
-                    icons={icons}
-                    value={options[0]}
-                    themeKey={themeKey}
-                    onChange={onChange}
-                />
-            );
-
-            cy.getByTestId('boolean-editor-toggle').should('exist').click();
-
-            cy.get('@onChange').should('have.been.calledOnce').and('have.been.calledWithExactly', options[1]);
-        });
+        cy.get('@onChange').should('have.been.calledOnce').and('have.been.calledWithExactly', options[1]);
     });
 });
