4275215e2c7f80ee92fd0da0d395d81d0ab5de67
Davit Yavryan <davit.yavryan@gmail.com>
2021-03-21 22:48:05 +0100

    build: simplify babel.config and remove data-testid from components

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,26 @@
-const defaultPresets = ['@babel/preset-typescript', '@babel/preset-react'];
+const target = process.env.TARGET || 'cjs';
 
-const env = process.env.BABEL_ENV || 'cjs';
-const isCommonJS = env === 'cjs';
+const presets = ['@babel/preset-typescript', '@babel/preset-react'];
 
 // We release a ES version of Material-UI Chameleon.
 // It's something that matches the latest official supported features of JavaScript.
 // Nothing more (stage-1, etc), nothing less (require, etc).
-if (process.env.BABEL_ENV !== 'es') {
-    defaultPresets.push([
-        '@babel/preset-env',
-        {
-            modules: isCommonJS ? 'commonjs' : false,
-        },
-    ]);
+if (target !== 'es') {
+    presets.push(['@babel/preset-env', { modules: target === 'cjs' ? 'commonjs' : false }]);
 }
 
 const plugins = [
+    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+    ['@babel/plugin-transform-runtime', { useESModules: ['es', 'esm'].includes(target) }],
+    '@babel/plugin-transform-object-assign', // for IE 11 support
     '@babel/plugin-transform-react-constant-elements',
+
     // See https://material-ui.com/guides/minimizing-bundle-size/
     [
         'babel-plugin-import',
         {
             libraryName: '@material-ui/core',
-            libraryDirectory: isCommonJS ? '' : 'esm',
+            libraryDirectory: target === 'cjs' ? '' : 'esm',
             camel2DashComponentName: false,
         },
         'core',
@@ -31,30 +29,18 @@ const plugins = [
         'babel-plugin-import',
         {
             libraryName: '@material-ui/icons',
-            libraryDirectory: isCommonJS ? '' : 'esm',
+            libraryDirectory: target === 'cjs' ? '' : 'esm',
             camel2DashComponentName: false,
         },
         'icons',
     ],
-    // ['babel-plugin-react-remove-properties', { properties: ['data-testid'] }], // TODO: enable later
 ];
 
+if (process.env.BABEL_ENV !== 'test') {
+    plugins.push(['babel-plugin-react-remove-properties', { properties: ['data-testid'] }]);
+}
+
 module.exports = {
-    presets: defaultPresets,
-    plugins: [
-        ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-transform-object-assign', // for IE 11 support
-    ],
-    env: {
-        cjs: {
-            plugins,
-        },
-        esm: {
-            plugins: [...plugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
-        },
-        es: {
-            plugins: [...plugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
-        },
-    },
+    presets,
+    plugins,
 };
